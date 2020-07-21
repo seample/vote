fce528b2cd001da0e152c25a099b806141b92a8b
davied nz <davoodnazarifard@gmai.com>
2020-07-21 18:22:07 +0430

    second commit

---
+++ main.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+   el:"#app",
+   data:{
+      message:"davied nz",
+     submissions:[ 
+      {
+         id:1,
+         author:'دماوند البرزی',
+         descrioptin:"وقتی صحبت از نمونه کار طراحان می‌شود؛ به این نکته توجه کنید که پروژه بزرگتر ضرورتاً پروژه بهتری نیست",
+         excerpt:"پنج نوع پروژه‌ای که نباید در نمونه کار خود لحاظ کنید",
+         vote_number:0,
+         author_img:"public/images/avatars/daniel.jpg",
+         post_img:"public/images/submissions/image-rose.png"
+      },{
+         id:2,
+         author:'دماوند البرزی',
+         descrioptin:"وقتی صحبت از نمونه کار طراحان می‌شود؛ به این نکته توجه کنید که پروژه بزرگتر ضرورتاً پروژه بهتری نیست",
+         excerpt:"پنج نوع پروژه‌ای که نباید در نمونه کار خود لحاظ کنید",
+         vote_number:0,
+         author_img:"public/images/avatars/daniel.jpg",
+         post_img:"public/images/submissions/image-rose.png"
+      
+    ]                      
+   }
+});
---
+++ seed.js
@@ -0,0 +1,38 @@
+const submistions = [
+   {
+      id:1,
+      author:'دماوند البرزی',
+      descrioptin:"وقتی صحبت از نمونه کار طراحان می‌شود؛ به این نکته توجه کنید که پروژه بزرگتر ضرورتاً پروژه بهتری نیست",
+      excerpt:"پنج نوع پروژه‌ای که نباید در نمونه کار خود لحاظ کنید",
+      vote_number:0,
+      author_img:"public/images/avatars/daniel.jpg",
+      post_img:"public/images/submissions/image-rose.png"
+   },
+   {
+      id:2,
+      author:'دماوند البرزی',
+      descrioptin:"وقتی صحبت از نمونه کار طراحان می‌شود؛ به این نکته توجه کنید که پروژه بزرگتر ضرورتاً پروژه بهتری نیست",
+      excerpt:"پنج نوع پروژه‌ای که نباید در نمونه کار خود لحاظ کنید",
+      vote_number:0,
+      author_img:"public/images/avatars/molly.jpg",
+      post_img:"public/images/submissions/image-rose.png"
+   },
+   {
+      id:3,
+      author:'دماوند البرزی',
+      descrioptin:"وقتی صحبت از نمونه کار طراحان می‌شود؛ به این نکته توجه کنید که پروژه بزرگتر ضرورتاً پروژه بهتری نیست",
+      excerpt:"پنج نوع پروژه‌ای که نباید در نمونه کار خود لحاظ کنید",
+      vote_number:0,
+      author_img:"public/images/avatars/veronika.jpg",
+      post_img:"public/images/submissions/image-rose.png"
+   },
+   {
+      id:4,
+      author:'دماوند البرزی',
+      descrioptin:"وقتی صحبت از نمونه کار طراحان می‌شود؛ به این نکته توجه کنید که پروژه بزرگتر ضرورتاً پروژه بهتری نیست",
+      excerpt:"پنج نوع پروژه‌ای که نباید در نمونه کار خود لحاظ کنید",
+      vote_number:0,
+      author_img:"public/images/avatars/kristy.jpg",
+      post_img:"public/images/submissions/image-rose.png"
+   },
+];
